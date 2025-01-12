d8ee726636e10d9d01615fbb937e811ae51de97a
mzfirozuddin <mz.firozuddin@gmail.com>
2025-01-12 21:47:49 +0530

    add async wrapper to handle error better way

--- src/category/category-routes.ts
+++ src/category/category-routes.ts
@@ -3,12 +3,13 @@ import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
 import { CategoryServices } from "./category-services";
 import logger from "../config/logger";
+import { asyncWrapper } from "../common/utils/asyncWrapper";
 
 const router = express.Router();
 
 const categoryService = new CategoryServices();
 const categoryController = new CategoryController(categoryService, logger);
-router.post("/", categoryValidator, categoryController.create);
+router.post("/", categoryValidator, asyncWrapper(categoryController.create));
 
 //: We can handle "this" reference error below way ( We have seen previously in "auth service"). But in catalog service we will bind this in controller
 // router.post(
