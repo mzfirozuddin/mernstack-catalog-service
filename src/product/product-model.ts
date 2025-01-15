b079c66a23ff805dc229a37ffd48876545a792f2
mzfirozuddin <mz.firozuddin@gmail.com>
2025-01-16 00:07:48 +0530

    add product model

---
+++ src/product/product-model.ts
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const priceConfigurationSchema = new mongoose.Schema({
+    priceType: {
+        type: String,
+        enum: ["base", "additional"],
+    },
+    availableOptions: {
+        type: Map, //: Map means key:value pare
+        of: Number,
+    },
+
+    //Eg:-
+    // availableOptions: {
+    //     "small": 400,  //: Here small is string and 400 is number
+    //     "medium": 600
+    // },
+});
+
+const attributeValueSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    value: {
+        type: mongoose.Schema.Types.Mixed,
+    },
+});
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            reqired: true,
+        },
+        description: {
+            type: String,
+            reqired: true,
+        },
+        image: {
+            type: String, //: S3 URL
+            reqired: true,
+        },
+        priceConfiguration: {
+            type: Map,
+            of: priceConfigurationSchema,
+        },
+        attributes: [attributeValueSchema],
+        tenantId: {
+            type: String,
+            reqired: true,
+        },
+        categoryId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+        },
+        isPublish: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export default mongoose.model("Product", productSchema);
