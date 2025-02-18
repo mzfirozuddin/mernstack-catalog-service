7f5a5e79ff3c8ad7c5d85643990800dc00a9d4c4
mzfirozuddin <mz.firozuddin@gmail.com>
2025-02-19 00:42:22 +0530

    configured cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import config from "config";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-routes";
 import productRouter from "./product/product-routes";
 import toppingRouter from "./topping/topping-routes";
 
 const app = express();
+app.use(
+    cors({
+        origin: [config.get("client.uri")],
+        credentials: true,
+    }),
+);
 
 //: Add middleware for handle JSON data
 app.use(express.json());
